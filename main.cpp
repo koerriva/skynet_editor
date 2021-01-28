b7a02b18a6546766ef625a9a56337cde720585c4
koerriva <rivakoer@gmail.com>
2021-01-29 00:11:28 +0800

    Init

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <raylib.h>
+#include <raymath.h>
 #include <glm/glm.hpp>
 #include <spdlog/spdlog.h>
 
@@ -10,27 +11,48 @@ void Input(){
 
 }
 
+bool IsInside(Vector2 center, Vector2 pos){
+    float x = pos.x - center.x;
+    float y = pos.y - center.y;
+    return 100.f>x*x+y*y;
+}
+
+int width = 1280,height=720;
+
 int main() {
     SetConfigFlags(FLAG_VSYNC_HINT | FLAG_MSAA_4X_HINT | FLAG_WINDOW_HIGHDPI);
-    InitWindow(1280,720,"Skynet Editor");
+    InitWindow(width,height,"Skynet Editor");
     SetWindowPosition(100,100);
     SetTargetFPS(60);
     Camera2D camera;
     camera.zoom = 1.0f;
+    camera.target = Vector2{0,0};
+    float radius = 10.0;
+    int row = width/20,col = height/20;
+    Vector2 entities[row*col];
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            entities[i*col+j].x = i*radius*2+radius;
+            entities[i*col+j].y = j*radius*2+radius;
+        }
+    }
     while(!WindowShouldClose()){
+        auto mPos = GetMousePosition();
+        auto wPos = GetScreenToWorld2D(mPos,camera);
+
         Input();
         BeginDrawing();
         ClearBackground(DARKBLUE);
         BeginMode2D(camera);
-        for (int i = 0; i < 10; ++i) {
-            Vector2 center;
-            for (int j = 0; j < 10; ++j) {
-                center.x = i*20.0f;
-                center.y = j*20.0f;
-                float radius = 10.0;
-                Color color = DARKGREEN;
-                DrawCircleV(center,radius,color);
+        for (auto center : entities) {
+            Color color;
+            if(IsInside(center,wPos)){
+                color = GREEN;
+            }else{
+                color = DARKGREEN;
             }
+
+            DrawCircleV(center,radius,color);
         }
         EndMode2D();
         DrawFPS(5,5);
@@ -38,12 +60,7 @@ int main() {
         camera.zoom +=GetMouseWheelMove();
         camera.zoom = clamp<float>(camera.zoom,1,5);
         DrawText(TextFormat("Mouse Zoom %2.f",camera.zoom),5,40,16,DARKBROWN);
-
-        auto mPos = GetMousePosition();
-        auto wPos = GetScreenToWorld2D(mPos,camera);
-
         DrawText(TextFormat("World Pos %2.f,%2.f",wPos.x,wPos.y),5,60,16,DARKBROWN);
-
         EndDrawing();
     }
     CloseWindow();
