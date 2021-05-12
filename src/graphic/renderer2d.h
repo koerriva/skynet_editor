23e12bfd8cfea1a870f746f85355abf739919169
koerriva <504097978@qq.com>
2021-05-12 19:07:39 +0800

    Graphic Abstraction

---
+++ src/graphic/renderer2d.h
@@ -0,0 +1,14 @@
+﻿//
+// Created by 50409 on 2021/5/12.
+//
+
+#ifndef SKYNET_EDITOR_RENDERER2D_H
+#define SKYNET_EDITOR_RENDERER2D_H
+
+namespace Graphic{
+    class Renderer2d{
+
+    };
+}
+
+#endif //SKYNET_EDITOR_RENDERER2D_H
