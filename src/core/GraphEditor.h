ff70263a90ea260915e0adfcd1ec9705ffa054eb
koerriva <504097978@qq.com>
2021-05-13 19:13:18 +0800

    Update GraphEditor.h

--- src/core/GraphEditor.h
+++ src/core/GraphEditor.h
@@ -6,9 +6,38 @@
 #define SKYNET_EDITOR_GRAPHEDITOR_H
 
 
+#include <raylib.h>
+
 namespace GamePlay{
     class GraphEditor {
-        
+
+    };
+
+    struct GraphNode{
+        int id;
+        Vector2 position{};
+        float radius=16;
+
+        //mouse event
+        bool cursorIn = false;
+        bool cursorOut = false;
+        //editor
+        int editType = 0;
+        int editColorType = 0;
+        bool editColorMode = false;
+        Color color{};
+
+        int parent;
+        Vector2 pinPosition={};
+        int children[8] = {};
+        int linkFrom=0;
+        int linkId=0;
+
+        GraphNode(){}
+        GraphNode(int id):id(id){}
+
+        void CursorIn(){ cursorIn = true;cursorOut= false;}
+        void CursorOut() {cursorIn = false;cursorOut = true;}
     };
 }
 
